aa3e9281e9a3b496660c93bbcb90d6f779807b86
3absy1 <yossfeabdaslm123@gmail.com>
2024-01-25 16:56:31 +0200

    product

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 use Illuminate\Http\Request;
 use App\Http\Controllers\CategoryController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AddressController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
@@ -28,7 +28,6 @@ Route::group([
     Route::get('/user-profile', [AuthController::class, 'userProfile']);
 });
 
-<<<<<<< HEAD
 //               Catogory
 Route::get('category',[CategoryController::class,'index']);
 
@@ -36,8 +35,7 @@ Route::get('category',[CategoryController::class,'index']);
 Route::post('address',[AddressController::class,'store']);
 Route::post('address_update',[AddressController::class,'update']);
 Route::get('address_show',[AddressController::class,'index']);
-=======
+
 //Product
 Route::get('/products',[ProductController::class,'index']);
 Route::get('/product/{id}',[ProductController::class,'show']);
->>>>>>> 0a995d579f13da049f694da6c379c47ee6d63421
