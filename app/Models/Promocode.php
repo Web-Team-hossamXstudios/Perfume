39cb92cc75cbcf52ee7d5024817a5a3c53a0e0a3
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-01-25 10:27:12 +0200

    f commit

--- app/Models/Promocode.php
+++ app/Models/Promocode.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Promocode extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
     
     public function orders( ){ 
         return $this->hasMany(Order::class); 
