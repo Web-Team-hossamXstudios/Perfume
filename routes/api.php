545a37f016b62f839a562e75a8a5e4c28bf3611b
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-01-29 09:54:33 +0200

    add

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,9 @@
 <?php
 
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\FavouriteController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderItemController;
->>>>>>> 4358e45d1c12c220e5eb96dbdd87534f381b8a5e
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -42,23 +39,14 @@ Route::group([
     Route::post('/update-profile',[AuthController::class, 'updateProfile']);
 });
 
-<<<<<<< HEAD
 //Catogory
 Route::get('category',[CategoryController::class,'index']);
 
-//Address
-Route::post('address',[AddressController::class,'store']);
-Route::post('address_update',[AddressController::class,'update']);
-Route::get('address_show',[AddressController::class,'index']);
-=======
 //Address
 Route::post('/address',[AddressController::class,'store']);
 Route::post('/address_update',[AddressController::class,'update']);
 Route::get('/address/{id}',[AddressController::class,'index']);
 
-//Category
-Route::get('/category',[CategoryController::class,'index']);
->>>>>>> 4358e45d1c12c220e5eb96dbdd87534f381b8a5e
 
 //Product
 Route::get('/products',[ProductController::class,'index']);
@@ -68,22 +56,14 @@ Route::get('/product/{id}',[ProductController::class,'show']);
 Route::get('/reviews/{id}',[ReviewController::class,'index']);
 Route::post('/reviews',[ReviewController::class,'store']);
 
-<<<<<<< HEAD
-//favourite
-Route::get('/favourites/{id}',[FavouriteController::class,'index']);
-=======
 //Favourite
 Route::get('/favourites',[FavouriteController::class,'show']);
->>>>>>> 4358e45d1c12c220e5eb96dbdd87534f381b8a5e
 Route::post('/favourites',[FavouriteController::class,'store']);
 
 //Cart
 Route::get('/carts',[CartController::class,'index']);
 Route::post('/carts',[CartController::class,'store']);
-<<<<<<< HEAD
-=======
 
 //order
 Route::post('/order',[OrderController::class,'store']);
 
->>>>>>> 4358e45d1c12c220e5eb96dbdd87534f381b8a5e
