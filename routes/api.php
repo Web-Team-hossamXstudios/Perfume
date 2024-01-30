bc66f0ace1e84970efe3ab713b8ed4111875b9f5
3absy1 <yossfeabdaslm123@gmail.com>
2024-01-30 09:48:24 +0200

    api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-=======
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\FavouriteController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderItemController;
->>>>>>> 4358e45d1c12c220e5eb96dbdd87534f381b8a5e
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -48,16 +41,7 @@ Route::get('category',[CategoryController::class,'index']);
 //Address
 Route::post('address',[AddressController::class,'store']);
 Route::post('address_update',[AddressController::class,'update']);
-Route::get('address_show',[AddressController::class,'index']);
-=======
-//Address
-Route::post('/address',[AddressController::class,'store']);
-Route::post('/address_update',[AddressController::class,'update']);
-Route::get('/address/{id}',[AddressController::class,'index']);
-
-//Category
-Route::get('/category',[CategoryController::class,'index']);
->>>>>>> 4358e45d1c12c220e5eb96dbdd87534f381b8a5e
+Route::get('address/{id}',[AddressController::class,'index']);
 
 //Product
 Route::get('/products',[ProductController::class,'index']);
@@ -67,13 +51,8 @@ Route::get('/product/{id}',[ProductController::class,'show']);
 Route::get('/reviews/{id}',[ReviewController::class,'index']);
 Route::post('/reviews',[ReviewController::class,'store']);
 
-<<<<<<< HEAD
 //favourite
 Route::get('/favourites/{id}',[FavouriteController::class,'index']);
-=======
-//Favourite
-Route::get('/favourites',[FavouriteController::class,'show']);
->>>>>>> 4358e45d1c12c220e5eb96dbdd87534f381b8a5e
 Route::post('/favourites',[FavouriteController::class,'store']);
 
 //Cart
@@ -82,4 +61,3 @@ Route::post('/carts',[CartController::class,'store']);
 
 //order
 Route::post('/order',[OrderController::class,'store']);
-
